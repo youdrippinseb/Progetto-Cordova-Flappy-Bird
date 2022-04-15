119d9a3d23f6df17ef6dd040d2811c8a1cbaaa3f
Sebastian Drago <sebastiandragomiristeanu@gmail.com>
2022-04-15 11:57:55 +0200

    v1.0.1
    base game tested and working on browser but still to add microphone support and fix height and graphic issues.

---
+++ DragoFlappyBird/FlappyBirdDrago/www/js/index.js
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+
+    hole.addEventListener('animationiteration', () => {
+        var random = -((Math.random()*300)+150);
+        hole.style.top = random + "px";
+        counter++;
+    });
+    setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if(jumping==0){
+            character.style.top = (characterTop+3)+"px";
+        }
+        var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+        var cTop = -(500-characterTop);
+        if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+            alert("Game over. Score: "+(counter-1));
+            character.style.top = 100 + "px";
+            counter=0;
+        }
+    },10);
+    
+    function jump(){
+        jumping = 1;
+        let jumpCount = 0;
+        var jumpInterval = setInterval(function(){
+            var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+            if((characterTop>6)&&(jumpCount<15)){
+                character.style.top = (characterTop-5)+"px";
+            }
+            if(jumpCount>20){
+                clearInterval(jumpInterval);
+                jumping=0;
+                jumpCount=0;
+            }
+            jumpCount++;
+        },10);
+    }
+
+}
+
+
